b3642e018287d59ef61df2db860eef1d8dbe5287
C1662458 <AlghamdiRS@cardiff.ac.uk>
2019-03-26 13:14:03 +0000

    few changes in JSON

--- Finals/Final.py
+++ Finals/Final.py
@@ -71,9 +71,8 @@ for triangle in getting_Triangles(G):
 
 for triangle in trianglesList:
     triangleCode = TRICODE_TO_NAME[tricode(G, triangle[0], triangle[1], triangle[2])]
-    jsonList.append({'x':int(triangle[0]), 'y':int(triangle[1]), 'z':int(triangle[2]), 'id':triangleCode,
-     'connections': [int(triangle[0]), int(triangle[1]), int(triangle[2])]})
-
+    jsonList.append({'x':int(triangle[0]), 'y':int(triangle[1]), 'z':int(triangle[2]), 'id':triangleCode})
+# 'connections': [int(triangle[0]), int(triangle[1]), int(triangle[2])]}
 
         # json.dump((triangle[0], triangle[1], triangle[2], triangleCode), json_file)
 with open('triads.json', 'a') as json_file:
